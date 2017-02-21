c71e2c4d962175bf3f5d8960e165275e0ead1e85
Jeffrey Corron <Jeffrey Corron>
2017-02-21 16:44:13 +0000

    support, events, contact pages

--- core/views.py
+++ core/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'core/home.html', {})
+	
+def support(request):
+	return render(request, 'core/support.html', {})
+
+def events(request):
+	return render(request, 'core/events.html', {})
+
+def contact(request):
+	return render(request, 'core/contact.html', {})
