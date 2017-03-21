72d85b31b418ee65f17f41c2e69879c59807d32c
Austin <austinpahl@gmail.com>
2017-03-20 20:34:55 -0400

    Fixed indentation in urls.py

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home_url'),
- 	url(r'^support/', views.support, name='support_url'),
- 	url(r'^contact/', views.contact, name='contact_url'),
- 	url(r'^events/', views.events, name='events_url'),
+    url(r'^support/', views.support, name='support_url'),
+    url(r'^contact/', views.contact, name='contact_url'),
+    url(r'^events/', views.events, name='events_url'),
     url(r'^projects$', views.projects, name='projects_url')
 ]
